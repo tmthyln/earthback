fc4fdcce9b3f4420de7c247df99b5e2c25d4a00a
Timothy Lin <tmthylin@gmail.com>
2018-12-15 22:22:29 -0500

    function to set background to image (bmp only

---
+++ load.py
@@ -0,0 +1,13 @@
+import ctypes
+import os
+
+
+def set_background(img_filename):
+    if not os.path.exists(img_filename):
+        raise ValueError('path to new background image must exist (can be relative)')
+
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, os.path.abspath(img_filename), 3)
+
+
+set_background('imgs/scottish.bmp')
+
